2c1881277741b8624269ae6973fa3315a641ce66
Mr.Dipendra Marasini <55050901+dipendramarasini@users.noreply.github.com>
2021-11-10 10:27:28 +0545

    Create slicing.js

---
+++ slicing.js
@@ -0,0 +1,3 @@
+let dummytext = "To use the extension, open the command palette (F1 or cmd/ctrl+shift+p, type  and select to insert either a line or paragraph."
+let data =  dummytext.slice(0,12);
+console.log(data);
